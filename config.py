991c5d313a82c668f3c5c5fd60f76036371cf08f
JackieWu <wujindou1@163.com>
2024-04-19 19:26:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     hidden_size = 768
     learning_rate = 2e-5 
     class_num = 5 
-    epoch = 2
+    epoch = 5
     train_file = './data/track1_train.txt'
     dev_file = './data/track1_dev.txt'
     test_file = './data/track1_test.txt'
