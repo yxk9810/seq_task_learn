72fe6757ddfc1ab939c1ae092ad71d4b4d187586
JackieWu <wujindou1@163.com>
2024-04-22 14:29:35 +0800

    Update train_and_eval.py

--- train_and_eval.py
+++ train_and_eval.py
@@ -91,9 +91,11 @@ def evaluate(model,dev_data_loader):
 
 
 dataset = ZhWikipediaDataSet(filepath=config.train_file)
-print('train data siz %d'.format(len(dataset)))
+print("train data siz {}".format(len(dataset)))
 train_data_loader = DataLoader(dataset, batch_size=8, collate_fn = collate_fn_wiki, shuffle=True)
 dev_dataset = ZhWikipediaDataSet(filepath=config.dev_file)
+print("train data siz {}".format(len(dev_dataset)))
+
 dev_data_loader =  DataLoader(dev_dataset, batch_size=4, collate_fn = collate_fn_wiki, shuffle=False)
 best_valid_loss = float('inf')
 for epoch in range(config.epoch):
